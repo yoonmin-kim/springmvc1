216925d18f30db329e728007452eceddf183dea2
YoonMin <rladbsals23@gmail.com>
2021-05-06 17:47:16 +0900

    feat : HTTP 응답 데이터 - API JSON
    콘텐트타입에 application/json을 설정해 준 뒤
    Jackson라이브러리의 ObjectMapper를 활용해서 출력해준다.
    
    [참고]
    application/json은 스펙상 utf-8을 사용하도록 정의되어있다.
    그래서 별도로 charset=utf-8을 설정하지 않아도된다.
    하지만 response.getWriter()를 사용하면 자동으로 utf-8을 추가한다.
    이때는 response.getOutputStream()을 사용하면 해결된다.

---
+++ src/main/java/hello/servlet/basic/reponse/ResponseJsonServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic.reponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("application/json");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(10);
+
+        String result = objectMapper.writeValueAsString(helloData);
+        res.getWriter().write(result);
+
+    }
+}
