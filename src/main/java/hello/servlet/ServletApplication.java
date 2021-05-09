be3626f1ddad1c6e67e3316bbef9ff3a9274540e
YoonMin <rladbsals23@gmail.com>
2021-05-09 19:49:57 +0900

    feat : 뷰 리졸버
    OldController에서 ModelAndView에 viewName으로 "new-form"을 설정하여 반환했다.
    
    뷰가 .jsp이기 때문에 스프링은 뷰 리졸버로 InternalResourceViewResolver를 사용하게 된다.
    InternalResourceViewResolver는 RequestDispatcher를 이용하여 jsp로 forward를 해준다.
    
    application.properties에 prefix, suffix를 등록해두면 스프링부트는
    InternalResourceViewResolver를 자동으로 등록한다.
    
    jsp가 아닌 타임리프와 같은 템플릿의 경우 html을 자바 코트도 모두 그려서
    HttpServletResponse에 등록해서 반환한다.

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	//prefix,suffix를 설정파일에 등록하지 않았을 경우 별도의 빈으로 등록해서 사용
+	@Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}
 }
